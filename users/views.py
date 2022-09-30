8c872ed094aa0a122ad3ad4a386d5deae9abf105
thoth <thoth2357@gmail.com>
2022-09-30 16:27:33 +0100

    pushed

--- users/views.py
+++ users/views.py
@@ -1,7 +1,7 @@
 import email
 from django.shortcuts import render
 from django.http import HttpResponse
-from .models import TeacherDetails, User, StudentDetails
+from .models import Student, TeacherDetails, User, StudentDetails
 
 from .forms import SignupForm,TeacherCreateForm
 
@@ -33,12 +33,17 @@ def PrincipalAddTeacher(request):
         'form': form,
     }
     if form.is_valid():
-        email = form.cleaned_data['email']
-        password = form.cleaned_data['password']
-        user_type = 'TEACHER'
-        Students = form.cleaned_data['Students']
-        new_user = User.objects.create_user(email=email, password=password, type=user_type)
-        new_teacher_details = TeacherDetails.objects.create(user=new_user)
-        new_teacher_details.user.add(*Students)
+        # try:
+            email = form.cleaned_data['email']
+            password = form.cleaned_data['password']
+            user_type = 'TEACHER'
+            Students = form.cleaned_data['Students']
+            new_user = User.objects.create_user(email=email, password=password, type=user_type)
+            new_teacher_details = TeacherDetails(user=new_user)
+            new_teacher_details.save()
+            print(Students, 'test', [student.id for student in Students])
+            new_teacher_details.Students.set([student.id for student in Students])
+            return HttpResponse("Success")
+            # return HttpResponse("Error",e)
     return render(request, 'create-teachers.html', context)
     
